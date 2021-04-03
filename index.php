f2bfb48a1ed0e16d7bd492b05b0278911eb3d328
anh kiet <ngovietanhkiet@gmail.com>
2021-04-03 14:24:43 +0700

    chuc nang

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+    require_once("./connection.php");
+    if (isset($_POST["btn_submit"])) {
+        $username   ="";
+        $password = "";
+		// lấy thông tin người dùng
+		$username = $_POST["txtname"];
+		$password = $_POST["txtpass"];
+		
+		if ($username == "admin" && $password =="admin") {
+			echo "dang nhap thanh cong";
+            header("Location: home.php");
+		}else{
+			$sql = "select * from user where Name = '$username' and Pass = '$password' ";
+			$query = mysqli_query($conn,$sql);
+			$num_rows = mysqli_num_rows($query);
+			if ($num_rows==0) {
+				echo "tên đăng nhập hoặc mật khẩu không đúng !";
+			}
+		}
+	}
+?>
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,9 +33,9 @@
 <body>
     <h2>Form Login</h2>
     <form action="" method="post">
-        UserName: <input type="text" name="name" /><br>
-        PassWord: <input type="password" name="pass" /><br>
-        <button name="btnlogin">Login</button>
+        UserName: <input type="text" name="txtname" />
+        PassWord: <input type="password" name="txtpass"/>
+        <button name="btn_submit">Login</button>
     </form>
 
 </body>
