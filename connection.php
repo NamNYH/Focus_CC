6261e30aff62a54505ae22564bd03ef1fd24be09
anh kiet <ngovietanhkiet@gmail.com>
2021-04-03 14:47:51 +0700

    sua lai connection

--- connection.php
+++ connection.php
@@ -8,6 +8,6 @@ if (!$conn) {
     die('Không thể kết nối: ' . mysqli_error($conn));
     exit();
 }
-echo 'Kết nối thành công';
-mysqli_close($conn);
+// echo 'Kết nối thành công';
+// mysqli_close($conn);
 ?>
